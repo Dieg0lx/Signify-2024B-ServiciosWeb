777569d2ef3019d453212910e8f65b210c55d249
CarlosQB <118304287+CarlosQB@users.noreply.github.com>
2024-10-02 08:39:48 -0600

    Add files via upload

---
+++ Signify-2024B-ServiciosWeb-main/Signify/src/controllers/userController.py
@@ -0,0 +1,36 @@
+from flask import render_template, request, redirect, url_for
+from app import db
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import text
+from model.usuario import Usuario
+
+def first():
+    try:
+        # Intentar ejecutar una consulta simple para verificar la conexión
+        # db.session.execute(text('SELECT 1'))
+        return render_template('login.html')
+    except OperationalError:
+        return "Vlio verga: Error de conexión a la base de datos"
+    
+def login():
+    if request.method == 'POST':
+        username = request.form.get('usuario')  # Nombre del campo en el formulario
+        password = request.form.get('contrasena')  # Nombre del campo en el formulario
+        print(username, password)
+        try:
+            user = Usuario.query.filter_by(nombre=username, contrasena=password).first()
+            if user:
+                return "Usuario encontrado"
+            else:
+                return "Usuario no encontrado"
+        except Exception as e:
+            print(e)
+    return render_template('indexContenidos.html')
+
+def second():
+    try:
+        # Intentar ejecutar una consulta simple para verificar la conexión
+        # db.session.execute(text('SELECT 1'))
+        return render_template('index.html')
+    except OperationalError:
+        return "Vlio verga: Error de renderizacion"
