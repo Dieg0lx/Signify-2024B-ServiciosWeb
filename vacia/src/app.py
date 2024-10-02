e18a7b9ef7de1c2e922033c880d3cc1c70b4f399
Diego <116047378+Dieg0lx@users.noreply.github.com>
2024-10-01 20:18:09 -0600

    Add files via upload

---
+++ vacia/src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/GestionUsuarios'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from routes.router import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
